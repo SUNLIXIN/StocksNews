f8206f43f25f25f794a364b97ffd01a3447b5d9a
Cameron <haodahaodaya@gmail.com>
2013-10-24 21:32:46 +0800

    add Login function

--- src/servlet/Login.java
+++ src/servlet/Login.java
@@ -1,17 +1,20 @@
 package servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.UserDAO;
+
 public class Login extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+			throws ServletException, IOException{
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
 
@@ -21,6 +24,29 @@ public class Login extends HttpServlet {
 			session.setAttribute("error", "All blanks should be filled!");
 			response.sendRedirect("login.jsp");
 			return;
+		} else {
+			try {
+				InitialContext context = new InitialContext();
+				UserDAO userDAO =context (UserDAO)context.lookup("UserDAO/local");
+				User user = userDAO.queryByName(name);
+				if (!user) {
+					session.setAttribute("error", "User doesn't exsit!");
+					response.sendRedirect("login.jsp");
+					return;
+				} else {
+					if (password != user.getPassword()) {
+						session.setAttribute("error", "Password Wrong!");
+						response.sendRedirect("login.jsp");
+						return;
+					} else {
+						session.setAttribute("user", user);
+						response.sendRedirect("index.jsp");
+						return;
+					}
+				}
+			} catch (NamingException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
